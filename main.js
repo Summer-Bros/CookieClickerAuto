34da70c11c33243938fca32b03720f19e5492810
Luanism <91706547+Luanism@users.noreply.github.com>
2022-12-06 19:41:32 -0600

    Create main.js
    Try doing something

---
+++ main.js
@@ -0,0 +1,2 @@
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+if(typeof cookieMonsterFrameworkData == 'undefined') Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js');
